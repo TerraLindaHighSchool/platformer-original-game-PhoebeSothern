ed9580ad3bc2234e5f43c10ddb10861b3591a0c0
Phoebe Sothern <ps01877@srcs.org>
2021-09-30 15:41:38 -0700

    Completed Tutorial 4

--- Level1.java
+++ Level1.java
@@ -31,7 +31,7 @@ public class Level1 extends World
         setPaintOrder(Player.class, Platform.class, Obstacle.class, Collectable.class, Door.class, HUD.class);
         Door door = new Door();
         addObject(door,1171,44);
-        Player player = new Player();
+        Player player = new Player(3,5.6f, GRAVITY, 3, 3, Level2.class, MUSIC);
         addObject(player,43,760);
         addObject(new Floor(), 600, 800);
         addObject(new SmallBrickWall(), 250, 550);
